7dff3cff241abfe21685db740cf8b814f4ef1c16
Hugo Peres <hugo.freitas@scania.com>
2017-05-04 09:50:56 -0300

    creating new branch

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,21 +7,16 @@ using NewCadeirinhaIoT.Parameters;
 using NewCadeirinhaIoT.PLC;
 using Windows.UI.Xaml;
 using System.Net;
-using NewCadeirinhaIoT.Models;
-using Windows.UI.Xaml.Shapes;
-using Windows.Storage;
 using NewCadeirinhaIoT.ViewModel;
 
 namespace NewCadeirinhaIoT
 {
-
     public partial class MainPage : Page
     {
         public MainPage()
         {
             InitializeComponent();
             DataContext = new MainViewModel();
-        }  
-
+        }
     }
 }
